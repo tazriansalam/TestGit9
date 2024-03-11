37ba8e6379a1cf418f075ba2407bc1efd340b5ab
tazriansalam <tazrian27@gmail.com>
2024-03-12 02:40:12 +0700

    changes

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+class Shoe:
+    def __init__(self, brand, model, size, price):
+        self.brand = brand
+        self.model = model
+        self.size = size
+        self.price = price
 
+    def display(self):
+        return f"{self.brand} {self.model} - Size: UK{self.size}, Price: ฿{self.price}"
+
+class ADIFOM_shoe(Shoe):
+    def __init__(self, brand, model, size, court, price):
+        super().__init__(brand, model, size, price)
+        self.court = court
+
+    def display(self):
+        return f"{self.brand} {self.model} - Size: UK{self.size}, Court: {self.court}, Price: ฿{self.pric
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     shoe1 = Shoe("Nike", "Air Max", 10, 150)
